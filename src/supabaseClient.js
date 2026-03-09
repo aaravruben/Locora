36cc62af4a41a0d150828a82c1d251d7b79f7f4f
Ruben Jacob <rubenshobby.jacob@gmail.com>
2026-03-08 18:19:21 -0700

    a

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,8 +4,3 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-const { data, error } = await supabase
-  .from("businesses")
-  .select("*");
-
-console.log(data);
