323a6824c6692729f5a0fe734f368ee543d0644e
Ruben Jacob <rubenshobby.jacob@gmail.com>
2026-03-08 18:53:44 -0700

    as

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/home";
 import BusinessPage from "./pages/BusinessPage";
 import About from "./pages/About";
@@ -6,20 +7,33 @@ import AddBusiness from "./pages/AddBusiness";
 import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <div>
+    <div className={darkMode ? "app dark" : "app light"}>
       <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/add-business">Add Business</Link>
-        <Link to="/about">About</Link>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/add-business">Add Business</Link>
+          <Link to="/about">About</Link>
+        </div>
+
+        <button
+          className="mode-btn"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          {darkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/business/:id" element={<BusinessPage />} />
-        <Route path="/add-business" element={<AddBusiness />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/business/:id" element={<BusinessPage />} />
+          <Route path="/add-business" element={<AddBusiness />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
     </div>
   );
 }
