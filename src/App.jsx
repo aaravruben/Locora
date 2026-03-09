a91bb71b8cfc98916ad8f8b35342645a4b257c45
Ruben Jacob <rubenshobby.jacob@gmail.com>
2026-03-08 18:44:48 -0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,22 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import BusinessPage from "./pages/BusinessPage";
 import About from "./pages/About";
+import AddBusiness from "./pages/AddBusiness";
 import "./App.css";
-import AddBusiness from "./AddBusiness";
-
-function App() {
-  return (
-    <div>
-      <Home />
-      <hr />
-      <AddBusiness />
-    </div>
-  );
-}
 
 function App() {
   return (
     <div>
       <nav className="nav">
         <Link to="/">Home</Link>
+        <Link to="/add-business">Add Business</Link>
         <Link to="/about">About</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/business/:id" element={<BusinessPage />} />
+        <Route path="/add-business" element={<AddBusiness />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
