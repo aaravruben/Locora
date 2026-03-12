5b53aa68f63e13084c5ab8ed40fbbcf37d735a85
wasd-cosmic <rubenshobby.jacob@gmail.com>
2026-03-12 20:55:00 +0000

    asasas

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://zvzqqrwaphonkofquapv.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2enFxcndhcGhvbmtvZnF1YXB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTM0ODgsImV4cCI6MjA4ODU4OTQ4OH0.NX9B46sizaY0vz08nsjQ7IBLXkZGjKAeiXxu3U2Ze0k";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
