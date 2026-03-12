ab4b338bedbb79970f0cede472121f7c61cb9ae1
Ruben Jacob <rubenshobby.jacob@gmail.com>
2026-03-11 18:45:50 -0700

    di

--- src/pages/Submit.jsx
+++ src/pages/Submit.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../supabase";
 
 function Submit() {
   const [name, setName] = useState("");
